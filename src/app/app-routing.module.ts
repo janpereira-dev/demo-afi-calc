2076ae56fc243041e56bd3198450a3ad672dcd06
Jan Antonio Pereira Zambrano <japereira@futurshealth.com>
2021-03-18 13:24:21 +0100

    006 - New AtomicComponent 'Page Body' and Bootstrap set in AngularJson

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BodyComponent } from '@pages';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', pathMatch: 'full', component: BodyComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', redirectTo: 'home', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
