c0d61236f20d4a5fcb102e8ae9c9ef96a99009f0
Jan Antonio Pereira Zambrano <japereira@futurshealth.com>
2021-03-18 12:58:23 +0100

    005 - Chance Tailwind for Bootstrap, and repair UnitTest

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,13 +23,6 @@ describe('AppComponent', () => {
   it(`should have as title 'demo-afi-calc'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('demo-afi-calc');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('demo-afi-calc app is running!');
+    expect(app.title).toEqual('AFI calculadora Digital');
   });
 });
