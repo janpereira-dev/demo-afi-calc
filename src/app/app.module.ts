e1e778038479a296ed8cf5876fdd87f5c4d95f45
Jan Antonio Pereira Zambrano <japereira@futurshealth.com>
2021-03-18 18:42:59 +0100

    007 - New Components NavBar and SideBar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BodyComponent } from './atomic/pages/body/body.component';
+import { NavbarComponent } from './atomic/template/navbar/navbar.component';
+import { SidebarComponent } from './atomic/template/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BodyComponent
+    BodyComponent,
+    NavbarComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/constants/constants.ts
@@ -0,0 +1,10 @@
+export const TYPES: Map<string, string> = new Map([
+  ['type1', 'Tipo N°1'],
+  ['type2', 'Tipo N°2'],
+  ['type3', 'Tipo N°3'],
+  ['type4', 'Tipo N°4'],
+  ['type5', 'Tipo N°5'],
+  ['type6', 'Tipo N°6']
+]);
+
+
